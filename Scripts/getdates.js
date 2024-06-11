befa920c74e4a9558a582d7ab0308456c84dde04
Shay Brown <y*********@gmail.com.password>
2024-06-11 11:43:44 -0600

    Week 3 Update

--- Scripts/getdates.js
+++ Scripts/getdates.js
@@ -3,3 +3,11 @@ let thisYear=today.getFullYear();
 let copyright="©" + thisYear + "\nShay Brown" + "\nMissouri, United States";
 document.getElementById("copyright").innerText=copyright;
 document.getElementById("lastModified").innerText="Last Modification: " + document.lastModified;
+
+const hamButton = document.getElementById("menu");
+const navigation = document.getElementById("navigation");
+
+hamButton.addEventListener('click', () => {  
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+  });
