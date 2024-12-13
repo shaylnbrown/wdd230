c9e0fd6c6c72d153c2dd32bf655278bd82210e7e
shaylnbrown <shaylnbrown@gmail.com>
2024-12-12 21:35:00 -0600

    Cleanup

--- lesson07/scripts/code.js
+++
@@ -1,5 +0,0 @@
-const today=new Date();
-let thisYear=today.getFullYear();
-let copyright="©" + thisYear + "\nShay Brown" + "\nWD 230 - Lazy Loading";
-document.getElementById("copyright").innerText=copyright;
-document.getElementById("lastModified").innerText="Last Modification: " + document.lastModified;
