c0e63e68bb31cf0559284b2d20b372b63a6a08a0
shaylnbrown <shaylnbrown@gmail.com>
2025-03-27 21:59:45 -0500

    weather home

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,15 +1,19 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const weatherCondition = document.getElementById("weatherConditions");
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=38.78&lon=-90.32&appid=708710d42c81822883148ad8ec2fd92e';
+//const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=708710d42c81822883148ad8ec2fd92e';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=38.83108&units=imperial&lon=-90.31689&appid=708710d42c81822883148ad8ec2fd92e';
+let data = "";
 
 async function apiFetch() {
     try {
       const response = await fetch(url);
       if (response.ok) 
-        {
-        const weatherData = await response.json();
+        {  
+              
+        data = await response.json();        
         console.log(data); 
       } 
       else 
@@ -20,18 +24,17 @@ async function apiFetch() {
     {
         console.log(error);
     }
-    displayResults(weatherData);
+    
+    displayResults(data);
+    
   }
 
   apiFetch();
 
-  function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
-    weatherIcon.setAttribute('alt', desc);
-    weatherIcon.setAttribute('src', iconsrc);
-    captionDesc.textContent = `${desc}`;
+  function displayResults(weatherInfo) {    
+    weatherText.innerHTML = `${weatherInfo.main.temp} &deg;F - ${weatherInfo.weather[0].description}`;    
+    weatherThumb.setAttribute('alt', weatherInfo.weather[0].description);
+    weatherThumb.setAttribute('src', `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`);    
   }
 
   
