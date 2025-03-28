923372e62d787761cb5eb497e4eca02898758a96
shaylnbrown <shaylnbrown@gmail.com>
2025-03-27 22:11:01 -0500

    update

--- scripts/links.js
+++ scripts/links.js
@@ -2,7 +2,7 @@ const weekList=document.getElementById("weeks");
 
 async function getLinks()
 {    
-    const response = await fetch("/data/links.json");
+    const response = await fetch("/wdd230/data/links.json");
     const data = await response.json();
     
     displayLinks(data.weeks);
